93299d8c31ff575dbaabbc9c885d7e935c49d26b
Sarthak Jain <jsarthak448@gmail.com>
2023-05-04 13:50:53 +0530

    fix: putBucketOwnershipControls applied
    Signed-off-by: Sarthak Jain <jsarthak448@gmail.com>

--- src/actions/uploadAction.ts
+++ src/actions/uploadAction.ts
@@ -24,6 +24,17 @@ export default async (bucketName: string, uploadDirectory: string, environmentPr
 		console.log(`S3 bucket does not exist. Creating ${bucketName}...`)
 		await S3.createBucket({ Bucket: bucketName }).promise()
 
+		await S3.putBucketOwnershipControls({
+			Bucket: bucketName,
+			OwnershipControls: {
+				Rules: [
+					{
+						ObjectOwnership: 'ObjectWriter'
+					}
+				]
+			}
+		}).promise()
+
 		console.log('Configuring bucket website...')
 		await S3.putBucketWebsite({
 			Bucket: bucketName,
