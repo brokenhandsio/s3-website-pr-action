35e96bc139cf2bae21192180c988615b77a8c36f
Tim <0xtimc@gmail.com>
2024-07-18 16:17:00 +0100

    Start updating dependencies

--- src/utils/deleteDeployments.ts
+++ src/utils/deleteDeployments.ts
@@ -15,41 +15,25 @@ export default async (
 			? `${environmentPrefix || 'PR-'}${github.context.payload.pull_request!.number}`
 			: `${environmentPrefix || 'ACTION-'}${dayjs().format('DD-MM-YYYY-hh:mma')}`
 
-	const deployments = await githubClient.graphql(
-		`
-      query GetDeployments($owner: String!, $repo: String!, $environments: [String!]) {
-        repository(owner: $owner, name: $repo) {
-          deployments(first: 100, environments: $environments) {
-            nodes {
-              id
-            }
-          }
-        }
-      }`,
-		{ ...repo, environments: [environment] }
-	)
-
-	const nodes = deployments.repository?.deployments?.nodes
+	const deployments = await githubClient.rest.repos.listDeployments({
+		...repo,
+		environment: environment,
+		per_page: 100
+	});
 
 	console.log(JSON.stringify(deployments))
 
-	if (nodes.length <= 0) {
+	if (deployments.data.length <= 0) {
 		console.log('No exiting deployments found for pull request')
 		return
 	}
 
-	for (const node of nodes) {
-		console.log(`Deleting existing deployment - ${node.id}`)
+	for (const deployment of deployments.data) {
+		console.log(`Deleting existing deployment - ${deployment.id}`)
 
-		await githubClient.graphql(
-			`
-          mutation DeleteDeployment($id: ID!) {
-            deleteDeployment(input: {id: $id} ) {
-              clientMutationId
-            }
-          }
-        `,
-			{ id: node.id }
-		)
+		await githubClient.rest.repos.deleteDeployment({
+			...repo,
+			deployment_id: deployment.id
+		})
 	}
 }
