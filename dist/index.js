e9d82e5264fe13e8fe3bb9893bbf0e1eaf82257f
Tim <0xtimc@gmail.com>
2024-07-20 23:58:12 +0100

    Make region explicit in client

--- dist/index.js
+++ dist/index.js
@@ -64293,13 +64293,39 @@ main();
 /***/ }),
 
 /***/ 68:
-/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const client_s3_1 = __nccwpck_require__(149);
-exports["default"] = new client_s3_1.S3Client();
+const core = __importStar(__nccwpck_require__(1838));
+exports["default"] = new client_s3_1.S3Client({
+    region: core.getInput('bucket-region')
+});
 
 
 /***/ }),
--- src/s3Client.ts
+++ src/s3Client.ts
@@ -1,3 +1,9 @@
 import { S3Client } from "@aws-sdk/client-s3"
+import * as core from '@actions/core'
 
-export default new S3Client()
+
+export default new S3Client(
+    { 
+        region: core.getInput('bucket-region')
+    }
+)
